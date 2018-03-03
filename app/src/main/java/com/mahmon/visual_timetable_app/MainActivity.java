99d4dea4b820cabff6dff0c70f1537cc76be286e
mahmon <wayne@mahmon.com>
2018-03-03 15:36:36 +0000

    Added top_action_bar to MainActivity

--- app/src/main/java/com/mahmon/visual_timetable_app/MainActivity.java
+++ app/src/main/java/com/mahmon/visual_timetable_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.mahmon.visual_timetable_app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
         // Animation override:
         // Go_in for this activity, go_out for previous activity
         overridePendingTransition(R.anim.go_in, R.anim.go_out);
+        // Implement top_action_bar as default action bar
+        Toolbar topActionBar = findViewById(R.id.top_action_bar);
+        setSupportActionBar(topActionBar);
     }
 
     // Method called onClick for button: btn_enter_app
