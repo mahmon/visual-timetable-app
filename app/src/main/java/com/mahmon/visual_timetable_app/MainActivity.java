aa1c880839a6eb3bb37300d60dcb120aa2c7774f
mahmon <wayne@mahmon.com>
2018-03-03 15:40:41 +0000

    Inflated top_action_bar_menu onto top_action_bar

--- app/src/main/java/com/mahmon/visual_timetable_app/MainActivity.java
+++ app/src/main/java/com/mahmon/visual_timetable_app/MainActivity.java
@@ -1,9 +1,11 @@
 package com.mahmon.visual_timetable_app;
 
+import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         // Implement top_action_bar as default action bar
         Toolbar topActionBar = findViewById(R.id.top_action_bar);
         setSupportActionBar(topActionBar);
+    }
+
+    // Implement the default options menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu topMenu) {
+        // Inflate the top_action_bar_menu onto top_action_bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_action_bar_menu, topMenu);
+        return true;
     }
 
     // Method called onClick for button: btn_enter_app
