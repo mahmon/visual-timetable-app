261f8c4842e263f5a1114edb5f9d52b480f35388
mahmon <wayne@mahmon.com>
2018-03-04 08:46:45 +0000

    Created bottom_action_bar_menu with add event icon button

--- app/src/main/java/com/mahmon/visual_timetable_app/MainActivity.java
+++ app/src/main/java/com/mahmon/visual_timetable_app/MainActivity.java
@@ -3,10 +3,6 @@ package com.mahmon.visual_timetable_app;
 import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,34 +14,6 @@ public class MainActivity extends AppCompatActivity {
         // Animation override:
         // Go_in for this activity, go_out for previous activity
         overridePendingTransition(R.anim.go_in, R.anim.go_out);
-        // Implement top_action_bar as default action bar for this activity
-        Toolbar topActionBar = findViewById(R.id.top_action_bar);
-        setSupportActionBar(topActionBar);
-    }
-
-    // Implement the default options menu
-    @Override
-    public boolean onCreateOptionsMenu(Menu topMenu) {
-        // Inflate the top_action_bar_menu onto top_action_bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.top_action_bar_menu, topMenu);
-        return true;
-    }
-
-    // Set method calls for items clicked in top_action_bar_menu
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Switch statement to manage menu user clicks
-        switch (item.getItemId()) {
-            // User clicked toggle_theme_button
-            case R.id.btn_toggle_theme:
-                // Method call from TopBarMethods
-                TopBarMethods.lightBulbClicked(this);
-                return true;
-            default:
-                // Invoke the superclass to handle unrecognised user action.
-                return super.onOptionsItemSelected(item);
-        }
     }
 
     // Method called onClick for button: btn_enter_app
