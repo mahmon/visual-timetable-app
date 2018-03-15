02d9fe41a89062eefbf9efd0414a707900fda51f
mahmon <wayne@mahmon.com>
2018-03-15 15:42:26 +0000

    Tidied up and commented Model classes

--- app/src/main/java/com/mahmon/visual_timetable_app/model/Event.java
+++ app/src/main/java/com/mahmon/visual_timetable_app/model/Event.java
@@ -2,41 +2,46 @@ package com.mahmon.visual_timetable_app.model;
 
 import com.google.firebase.database.Exclude;
 
+// Class used to instantiate Event objects for storage in database
 public class Event {
+
+    // Variables for name, image url and database key
     private String mName;
     private String mImageUrl;
     private String mKey;
 
+    // Dfault constructor required by Firebase
     public Event() {
-        //empty constructor needed
     }
 
+    // Constructor to build event objects, pass name and imageUrl
     public Event(String name, String imageUrl) {
         mName = name;
         mImageUrl = imageUrl;
     }
 
+    // Getter methods
     public String getName() {
         return mName;
     }
-
-    public void setName(String name) {
-        mName = name;
-    }
-
     public String getImageUrl() {
         return mImageUrl;
     }
 
+    // Setter methods
+    public void setName(String name) {
+        mName = name;
+    }
     public void setImageUrl(String imageUrl) {
         mImageUrl = imageUrl;
     }
 
+    // Getter and setter for Key
+    // Excluded from Firebase to avoid duplicate data
     @Exclude
     public String getKey() {
         return mKey;
     }
-
     @Exclude
     public void setKey(String key) {
         mKey = key;
