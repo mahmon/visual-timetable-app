6f22fcc1262d08c941dab98f7aee53c7b0d6d5d1
mahmon <wayne@mahmon.com>
2018-03-29 18:36:23 +0100

    Theme colours toggle on switch toggle, start activity only

--- app/src/main/java/com/mahmon/visual_timetable_app/view/StartActivity.java
+++ app/src/main/java/com/mahmon/visual_timetable_app/view/StartActivity.java
@@ -1,5 +1,7 @@
 package com.mahmon.visual_timetable_app.view;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,21 +15,34 @@ import com.mahmon.visual_timetable_app.R;
 // Class for Login screen
 public class StartActivity extends BaseActivity {
 
+    private static final String PREFS_NAME = "prefs";
+    private static final String PREF_DARK_THEME = "dark_theme";
+
     private Switch mSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Use the chosen theme
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean useDarkTheme = preferences.getBoolean(PREF_DARK_THEME, false);
+
+        if(useDarkTheme) {
+            setTheme(R.style.AppTheme_Dark_NoActionBar);
+        }
+
         super.onCreate(savedInstanceState);
         // Link this activity to the relevant XML layout
         setContentView(R.layout.activity_start);
 
         // Theme switch...
         mSwitch = findViewById(R.id.switch_theme);
+        mSwitch.setChecked(useDarkTheme);
         // Listener
         mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                toggleTheme();
+                toggleTheme(isChecked);
             }
         });
 
@@ -42,6 +57,17 @@ public class StartActivity extends BaseActivity {
         getToolBarBottom().getMenu().removeItem(R.id.btn_delete_event);
         // Animation override:
         overridePendingTransition(R.anim.slide_in, R.anim.shrink_out);
+    }
+
+    private void toggleTheme(boolean darkTheme) {
+        SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putBoolean(PREF_DARK_THEME, darkTheme);
+        editor.apply();
+
+        Intent intent = getIntent();
+        finish();
+
+        startActivity(intent);
     }
 
     // Implement the default options menu
